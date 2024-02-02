738defb09fbedb5e76a6be13d70b692e7a78445f
ZAlexanderV <alexandr_zft@mail.ru>
2024-02-02 16:31:03 +0600

    Ansible update

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_network_name" {
+  # Описание переменной
+  description = "YC VPC Network name"
+  default     = "app-network"
+  type        = string
+}
+
+variable "vpc_subnet_name" {
+  # Описание переменной
+  description = "YC Subnet name"
+  default     = "app-subnet"
+  type        = string
+}
+
+variable "zone" {
+  description = "YC Zone ID"
+  default     = "ru-central1-a"
+  validation {
+    condition     = var.zone == null ? true : contains(["ru-central1-a", "ru-central1-b", "ru-central1-d"], var.zone)
+    error_message = "Only a valid YC region names are expected here such as ru-central1-a."
+  }
+}
+
+variable "yc_folder_id" {
+  description = "YC Folder ID"
+  default     = null
+}
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,10 +1,3 @@
-provider "yandex" {
-  service_account_key_file = var.service_account_key_file
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
-}
-
 module "vpc" {
   source       = "../modules/vpc"
   yc_folder_id = var.folder_id
